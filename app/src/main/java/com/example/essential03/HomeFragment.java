64063446837c5e9c2fdec2abac7a2b29439c07a4
hyeju <20211403@sungshin.ac.kr>
2022-10-12 23:37:41 +0900

    git commit --amend

--- app/src/main/java/com/example/essential03/HomeFragment.java
+++ app/src/main/java/com/example/essential03/HomeFragment.java
@@ -20,8 +20,8 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static UserFragment newInstance(String param1, String param2) {
-        UserFragment fragment = new UserFragment();
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -42,6 +42,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
