6ea6340705e48c47553522616fb635fa2fdd1e3b
simmee <20211002@sungshin.ac.kr>
2022-11-08 23:13:25 +0900

    git commit --amend

--- app/src/main/java/com/example/essential03/RankFragment.java
+++ app/src/main/java/com/example/essential03/RankFragment.java
@@ -1,5 +1,6 @@
 package com.example.essential03;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,7 @@ public class RankFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button addButton;
 
     public RankFragment() {
         // Required empty public constructor
@@ -59,6 +62,14 @@ public class RankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
         return inflater.inflate(R.layout.fragment_rank, container, false);
     }
 }
