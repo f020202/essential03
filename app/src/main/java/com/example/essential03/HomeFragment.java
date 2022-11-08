f9ff19921a3638912c27750403b0edd5f475a3fe
hyeju <20211403@sungshin.ac.kr>
2022-11-08 21:28:04 +0900

    git commit --amend

--- app/src/main/java/com/example/essential03/HomeFragment.java
+++ app/src/main/java/com/example/essential03/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.essential03;
 import static android.content.Context.MODE_NO_LOCALIZED_COLLATORS;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.FileInputStream;
@@ -22,11 +25,11 @@ import java.io.FileOutputStream;
 public class HomeFragment extends Fragment {
 
     //캘린더 관련
-    public String readDay = null;
-    public String str = null;
+    public String fname=null;
+    public String str=null;
     public CalendarView calendarView;
-    public Button cha_Btn, del_Btn, save_Btn;
-    public TextView diaryTextView, textView2, textView3;
+    public ImageView save_Btn,cha_Btn,del_Btn;
+    public TextView diaryTextView,textView2,textView3;
     public EditText contextEditText;
 
     private static final String ARG_PARAM1 = "param1";
@@ -57,44 +60,46 @@ public class HomeFragment extends Fragment {
         }
     }
 
+
+    //캘린더 여기서부터 시작
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        calendarView = v.findViewById(R.id.calendarView);
-        diaryTextView = v.findViewById(R.id.diaryTextView);
+        calendarView= v.findViewById(R.id.calendarView);
+        diaryTextView= v.findViewById(R.id.diaryTextView);
         save_Btn = v.findViewById(R.id.save_Btn);
         del_Btn = v.findViewById(R.id.del_Btn);
-        cha_Btn = v.findViewById(R.id.cha_Btn);
-        textView2 = v.findViewById(R.id.textView2);
-        textView3 = v.findViewById(R.id.textView3);
-        contextEditText = v.findViewById(R.id.contextEditText);
+        cha_Btn=v.findViewById(R.id.cha_Btn);
+        textView2=v.findViewById(R.id.textView2);
+        textView3=v.findViewById(R.id.textView3);
+        contextEditText=v.findViewById(R.id.contextEditText);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
-        {
+        Intent intent=getActivity().getIntent();
+        String name=intent.getStringExtra("userName");
+        final String userID=intent.getStringExtra("userID");
+        textView3.setText(name+"님, 안녕하세요");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth)
-            {
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 diaryTextView.setVisibility(View.VISIBLE);
                 save_Btn.setVisibility(View.VISIBLE);
                 contextEditText.setVisibility(View.VISIBLE);
                 textView2.setVisibility(View.INVISIBLE);
                 cha_Btn.setVisibility(View.INVISIBLE);
                 del_Btn.setVisibility(View.INVISIBLE);
-                diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth));
+                diaryTextView.setText(String.format("%d년 %d월 %d일",year,month+1,dayOfMonth));
                 contextEditText.setText("");
-                checkDay(year, month, dayOfMonth);
+                checkDay(year,month,dayOfMonth,userID);
             }
         });
-
-        save_Btn.setOnClickListener(new View.OnClickListener()
-        {
+        save_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                saveDiary(readDay);
-                str = contextEditText.getText().toString();
+            public void onClick(View view) {
+                saveDiary(fname);
+                str=contextEditText.getText().toString();
                 textView2.setText(str);
                 save_Btn.setVisibility(View.INVISIBLE);
                 cha_Btn.setVisibility(View.VISIBLE);
@@ -108,20 +113,18 @@ public class HomeFragment extends Fragment {
         return v;
     }
 
-    public void checkDay(int cYear, int cMonth, int cDay)
-    {
-        readDay = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt";
-        FileInputStream fis;
+    public void  checkDay(int cYear,int cMonth,int cDay,String userID){
+        fname=""+userID+cYear+"-"+(cMonth+1)+""+"-"+cDay+".txt";//저장할 파일 이름설정
+        FileInputStream fis=null;//FileStream fis 변수
 
-        try
-        {
-            fis = getActivity().openFileInput(readDay);
+        try{
+            fis=getActivity().openFileInput(fname);
 
-            byte[] fileData = new byte[fis.available()];
+            byte[] fileData=new byte[fis.available()];
             fis.read(fileData);
             fis.close();
 
-            str = new String(fileData);
+            str=new String(fileData);
 
             contextEditText.setVisibility(View.INVISIBLE);
             textView2.setVisibility(View.VISIBLE);
@@ -131,11 +134,9 @@ public class HomeFragment extends Fragment {
             cha_Btn.setVisibility(View.VISIBLE);
             del_Btn.setVisibility(View.VISIBLE);
 
-            cha_Btn.setOnClickListener(new View.OnClickListener()
-            {
+            cha_Btn.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view)
-                {
+                public void onClick(View view) {
                     contextEditText.setVisibility(View.VISIBLE);
                     textView2.setVisibility(View.INVISIBLE);
                     contextEditText.setText(str);
@@ -147,22 +148,19 @@ public class HomeFragment extends Fragment {
                 }
 
             });
-            del_Btn.setOnClickListener(new View.OnClickListener()
-            {
+            del_Btn.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view)
-                {
+                public void onClick(View view) {
                     textView2.setVisibility(View.INVISIBLE);
                     contextEditText.setText("");
                     contextEditText.setVisibility(View.VISIBLE);
                     save_Btn.setVisibility(View.VISIBLE);
                     cha_Btn.setVisibility(View.INVISIBLE);
                     del_Btn.setVisibility(View.INVISIBLE);
-                    removeDiary(readDay);
+                    removeDiary(fname);
                 }
             });
-            if (textView2.getText() == null)
-            {
+            if(textView2.getText()==null){
                 textView2.setVisibility(View.INVISIBLE);
                 diaryTextView.setVisibility(View.VISIBLE);
                 save_Btn.setVisibility(View.VISIBLE);
@@ -171,48 +169,37 @@ public class HomeFragment extends Fragment {
                 contextEditText.setVisibility(View.VISIBLE);
             }
 
-        }
-        catch (Exception e)
-        {
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
-
     @SuppressLint("WrongConstant")
-    public void removeDiary(String readDay)
-    {
-        FileOutputStream fos;
-        try
-        {
-            fos = getActivity().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS);
-            String content = "";
+    public void removeDiary(String readDay){
+        FileOutputStream fos=null;
+
+        try{
+            fos=getActivity().openFileOutput(readDay,MODE_NO_LOCALIZED_COLLATORS);
+            String content="";
             fos.write((content).getBytes());
             fos.close();
 
-        }
-        catch (Exception e)
-        {
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
-
     @SuppressLint("WrongConstant")
-    public void saveDiary(String readDay)
-    {
-        FileOutputStream fos;
-        try
-        {
-            fos = getActivity().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS);
-            String content = contextEditText.getText().toString();
+    public void saveDiary(String readDay){
+        FileOutputStream fos=null;
+
+        try{
+            fos=getActivity().openFileOutput(readDay,MODE_NO_LOCALIZED_COLLATORS);
+            String content=contextEditText.getText().toString();
             fos.write((content).getBytes());
             fos.close();
-        }
-        catch (Exception e)
-        {
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
-
 
 
 
